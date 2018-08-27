b261c96da6abaff4bd52db6d22b4cdc3f26135f1
Yohanan <harrochy96@gmail.com>
2018-08-27 17:39:21 +0200

    set up app for heroku 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://Aslatiel:REDreaper96@ds161710.mlab.com:61710/todo');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://Aslatiel:REDreaper96@ds161710.mlab.com:61710/todo');
 
 
 
